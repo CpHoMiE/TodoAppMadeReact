42df9c3ede7dcc067794f2f8bc876b38d67a4df3
강문준 <9929kmj@naver.com>
2019-06-13 10:20:42 +0900

    CRUD ToDo Data From AsyncStorage And Finished This Study!

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView, AsyncStorage } from 'react-native';
 import ToDo from './ToDo'
+import {AppLoading} from 'expo';
+import uuidv1 from 'uuid/v1'
 
 const {height, width} = Dimensions.get("window");
 
 export default class App extends React.Component{
 
+  /* newTodoData: 새로 추가할 ToDo 작성중인 데이터 | loadedToDos: ToDo 앱 실행여부 | toDos:state에 저장된 ToDo Array*/
   state={
-    newTodoData:""
+    newTodoData:"",
+    loadedToDos:false,
+    toDos:{}
   };
 
-  render(){
-    const{newTodoData} = this.state;
+  componentDidMount = () =>{
+    this._loadToDoApp();
+  }
 
+  render(){
+    const{newTodoData, loadedToDos, toDos} = this.state;
+
+    if(!loadedToDos){
+      return <AppLoading/>;
+    }
+    
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content"/>
@@ -25,22 +38,137 @@ export default class App extends React.Component{
           onChangeText={this._handleNewTodo} 
           placeholderTextColor={"#999"}
           returnKeyType={"done"}
-          autoCorrect={false}/>
+          autoCorrect={false}
+          onSubmitEditing={this._addTodo}/>
           <ScrollView contentContainerStyle={styles.toDos}>
-            <ToDo />
-            <ToDo />
-            <ToDo />
+            {Object.values(toDos).reverse().map(toDoObj=>(
+              <ToDo key={toDoObj.id} {...toDoObj} deleteToDo={this._deleteToDo} completeToDo={this._completeToDo} uncompleteToDo={this._uncompleteToDo} updateToDo={this._updateToDo}/>
+            ))}
           </ScrollView>
         </View>
       </View>
     );
   };
 
+  _loadToDoApp = async ()=>{
+    try{
+      //await을 통해 해당 작업을 마칠때 까지 wait 함.
+      const loadToDos = await AsyncStorage.getItem("toDos");
+      const toDosFromJson = JSON.parse(loadToDos);
+      this.setState({
+        loadedToDos:true,
+        toDos:toDosFromJson
+      });
+    }catch(err){
+      //AsyncStorage Data Loading Failed
+      console.log(err);
+    }
+  }
+
   _handleNewTodo=text=>{
     this.setState({
       newTodoData:text
     });
-    (Platform.OS==='android' ? console.log("android로부터 작성중인 input : "+text):console.log("ios로부터 작성중인 input : "+text))
+  }
+
+  _addTodo = ()=>{
+    const {newTodoData} = this.state;
+    if(newTodoData!==""){
+      this.setState({
+        newTodoData:""
+      });
+
+      this.setState(prevState=>{
+        const UUID = uuidv1();
+        const newToDoObj = {
+          [UUID]:{
+            id:UUID,
+            isCompleted:false,
+            toDoText:newTodoData,
+            createdTime:Date.now()
+          }
+        }
+        const newState={
+          ...prevState,
+          newTodoData:"",
+          toDos:{
+            ...prevState.toDos,
+            ...newToDoObj
+          }
+        }
+        this._saveTodos(newState.toDos);
+        return {...newState}
+      });
+    }
+  }
+
+  _deleteToDo=(id)=>{
+    this.setState(prevState=>{
+      const toDos = prevState.toDos;
+      delete toDos[id];
+      const newState = {
+        ...prevState,
+        ...toDos
+      };
+      this._saveTodos(newState.toDos);
+      return {...newState};
+    });
+  }
+
+  _completeToDo=(id)=>{
+    this.setState(prevState=>{
+      const newState={
+        ...prevState,
+        toDos:{
+          ...prevState.toDos,
+          [id]:{
+            ...prevState.toDos[id],
+            isCompleted:true
+          }
+        }
+      };
+      this._saveTodos(newState.toDos);
+      return {...newState};
+    });
+  }
+
+  _uncompleteToDo=(id)=>{
+    this.setState(prevState=>{
+      const newState={
+        ...prevState,
+        toDos:{
+          ...prevState.toDos,
+          [id]:{
+            ...prevState.toDos[id],
+            isCompleted:false
+          }
+        }
+      };
+      this._saveTodos(newState.toDos);      
+      return {...newState};
+    });
+  }
+
+  _updateToDo=(id, text)=>{
+    this.setState(prevState=>{
+      const newState={
+        ...prevState,
+        toDos:{
+          ...prevState.toDos,
+          [id]:{
+            ...prevState.toDos[id],
+            toDoText:text
+          }
+        }
+      };
+      this._saveTodos(newState.toDos);
+      return {...newState};
+    });
+  }
+
+  _saveTodos=(newToDos)=>{
+    //Object Array To Json Array
+    const saveToDos = AsyncStorage.setItem("toDos",JSON.stringify(newToDos));
   }
 }
 
--- ToDo.js
+++ ToDo.js
@@ -1,37 +1,160 @@
 import React, {Component} from "react";
-import {View, Text, TouchableOpacity, StyleSheet, Dimensions} from "react-native";
+import {View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput, Alert} from "react-native";
+import {Foundation} from '@expo/vector-icons';
+import PropTypes from "prop-types";
 
 const {width, height} = Dimensions.get("window");
 
 export default class ToDo extends Component{
 
-    /* isEditing:To do 수정중 | isCompleted:To do 작성 완료 */
-    state={
-        isEditing : false,
-        isCompleted: false
-    };
+    /* isEditing:To do 수정중 | toDoValue:To do에 대해 수정중인 데이터*/
+    constructor(props){
+        super(props);
+        this.state = {
+            isEditing:false, 
+            toDoValue:this.props.toDoText,
+            lastPress:0,
+        };
+    }
+
+    static propTypes={
+        id:PropTypes.string.isRequired,
+        toDoText: PropTypes.string.isRequired,
+        isCompleted: PropTypes.bool.isRequired,
+        createdTime:PropTypes.number.isRequired,
+        deleteToDo:PropTypes.func.isRequired,
+        completeToDo:PropTypes.func.isRequired,
+        uncompleteToDo:PropTypes.func.isRequired,
+        updateToDo:PropTypes.func.isRequired,
+    }
 
     render(){
-        const {isCompleted} = this.state;
+        const {isEditing, toDoValue, lastPress} = this.state;
+        const {toDoText,isCompleted, id, deleteToDo} = this.props;
 
         return(
             <View style={styles.container}>
-                <TouchableOpacity onPress={this._completeToDo}>
+               <View style={styles.column}>
+                    <TouchableOpacity onPress={this._compToggleCheck}>
                     <View style={[
                         styles.circle, 
                         isCompleted? styles.completedCircle : styles.uncompletedCircle
                     ]}/>
-                </TouchableOpacity>
-                <Text style={styles.todoTitle}>Is to do.</Text>
+                    </TouchableOpacity>
+                    {isEditing ? <TextInput 
+                        style={[
+                            styles.inputEditToDo,
+                            styles.todoTitle,
+                            isCompleted? styles.completedTitleEditing : styles.uncompletedTitle
+                        ]} 
+                        value={toDoValue} 
+                        multiline={true}
+                        onChangeText={this._handleInputToDo}
+                        onBlur={this._finishEditing}
+                        returnKeyType={"done"}/>
+                        :<Text style={[
+                        styles.todoTitle,
+                        isCompleted? styles.completedTitle : styles.uncompletedTitle
+                        ]} onPress={()=>{
+                            var delta = new Date().getTime() - lastPress;
+                            
+                            if(delta<200){
+                                // double tap detected
+                                this._compToggleCheckDoubleClick.call()
+                            }
+                            
+                            this.setState({
+                                lastPress:new Date().getTime()
+                            });
+                        }}>{toDoText}</Text>}
+               </View>
+                    {isEditing ? (
+                        <View style={styles.actions}>
+                            <TouchableOpacity onPressOut={this._finishEditing}>
+                                <View style={styles.actionContainer}>
+                                    <Foundation name={"check"} size={20} color={"green"}/>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                     ):(<View style={styles.actions}>
+                        <TouchableOpacity onPressOut={this._startEditing}>
+                            <View style={styles.actionContainer}>
+                                <Foundation name={"pencil"} size={20} color={"gray"}/>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPressOut={(event)=>{
+                            event.stopPropagation();
+                            Alert.alert(
+                                '삭제',
+                                '다음 ToDO를 삭제하시겠습니까?',
+                                [
+                                    {
+                                        text:"No", 
+                                        style:'cancel',
+                                    },
+                                    {text:'yes',onPress:()=>
+                                        {
+                                            deleteToDo(id)
+                                        }
+                                    },
+                                ],
+                                {cancelable:false}
+                            )
+                        }}>
+                            <View style={styles.actionContainer}>
+                                <Foundation name={"x"} size={20} color={"red"}/>
+                            </View>
+                        </TouchableOpacity>
+                    </View>)}
             </View>
         );
     }
 
-    _completeToDo = ()=>{
-        this.setState(prevState=>{
-            return{
-                isCompleted:!prevState.isCompleted
-            }
+    _compToggleCheck = (event)=>{
+        event.stopPropagation();
+        const {id, isCompleted, completeToDo, uncompleteToDo} = this.props;
+
+        if(isCompleted){
+            uncompleteToDo(id);
+        }else{
+            completeToDo(id);
+        }
+    }
+
+    _compToggleCheckDoubleClick = ()=>{
+        const {id, isCompleted, completeToDo, uncompleteToDo} = this.props;
+
+        if(isCompleted){
+            uncompleteToDo(id);
+        }else{
+            completeToDo(id);
+        }
+    }
+
+    _startEditing=(event)=>{
+        event.stopPropagation();
+        this.setState({
+            isEditing:true,
+        })
+    }
+
+    _finishEditing=(event)=>{
+        event.stopPropagation();
+        const {toDoValue} = this.state;
+        const {id,updateToDo, toDoText} = this.props;
+
+        if(toDoValue!=="" && toDoText!==toDoValue){
+            updateToDo(id,toDoValue);
+        }
+
+        this.setState({
+            isEditing:false
+        })
+    }
+    
+    _handleInputToDo = (toDoText)=>{
+        this.setState({
+            toDoValue:toDoText
         });
     }
 }
@@ -42,27 +165,54 @@ const styles = StyleSheet.create({
         borderBottomColor:"#bbb",
         borderBottomWidth: 1,
         flexDirection:"row",
-        alignItems:"center"
+        alignItems:"center",
+        justifyContent:"space-between"
     },
     todoTitle:{
         fontSize:24,
-        padding:10,
+        marginVertical:15,
         fontWeight:"400",
-        marginLeft:7
+        marginLeft:7,
+        width:(width/2)+50
+    },
+    completedTitle:{
+        color:"#bbb",
+        textDecorationLine:"line-through"
+    },
+    completedTitleEditing:{
+        color:"#bbb",
+    },
+    uncompletedTitle:{
+        color:"#F23657"
     },
     circle:{
-        width:33,
-        height:33,
+        width:28,
+        height:28,
         borderRadius:25,
         borderWidth:2,
         marginLeft:12
     },
     completedCircle:{
-        borderColor:"#0000fb",
-        backgroundColor:"#0000fb"
+        borderColor:"#bbb",
+        backgroundColor:"#bbb"
     },
     uncompletedCircle:{
         borderColor:"#F23657",
         backgroundColor:"white"
+    },
+    column:{
+        flexDirection:"row",
+        alignItems:"center",
+        width:width/2
+    },
+    actions:{
+        flexDirection:"row"
+    },
+    actionContainer:{
+        marginVertical:10,
+        marginHorizontal:10
+    },
+    inputEditToDo:{
+        paddingBottom:5,
     }
 });
